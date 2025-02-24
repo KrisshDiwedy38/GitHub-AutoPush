a9d1690f3fb7116f06ae5749d1894c7c80ca391f
Krissh Diwedy <krisshdiwedy38@gmail.com>
2025-02-24 20:54:43 +0530

    First Commit

---
+++ GitHub_AutoCommit.py
@@ -0,0 +1,30 @@
+import requests
+from GitHub_PushToken import PushingToken
+
+def get_headers(token): 
+   return {
+         'Authorization': f'Bearer {token}',
+         "Accept": "application/vnd.github+json",
+         "X-GitHub-Api-Version": "2022-11-28"
+      }
+
+def getData(token):
+   username = "KrisshDiwedy38"
+   repo_name = "GitHub-Management"
+   url = f"https://api.github.com/repos/{username}/{repo_name}/contents"
+   headers = get_headers(token)
+   
+   
+   response = requests.get(url , headers=headers)
+
+   if response.status_code == 200:
+      print(response.json())
+   else:
+      print(f"Could not get data" , response.status_code)
+   
+
+def main(token):
+   getData(token)
+
+if __name__ == "__main__":
+   main(PushingToken)
