b50a3c93bdcc01833fbaa17dcffef02591b16aa3
Krissh Diwedy <krisshdiwedy38@gmail.com>
2025-03-18 20:41:04 +0530

    Done with checking from changes in directory

--- GitHub_AutoCommit.py
+++ GitHub_AutoCommit.py
@@ -1,31 +1,54 @@
-import requests
+import os 
+import sys
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import tkinter as tk
+from tkinter import simpledialog, messagebox
 from GitHub_PushToken import PushingToken
 
-def get_headers(token): 
-   return {
-         'Authorization': f'Bearer {token}',
-         "Accept": "application/vnd.github+json",
-         "X-GitHub-Api-Version": "2022-11-28"
-      }
 
-def getData(token):
-   username = "KrisshDiwedy38"
-   repo_name = "GitHub-Management"
-   url = f"https://api.github.com/repos/{username}/{repo_name}/contents"
-   headers = get_headers(token)
-   
-   
-   response = requests.get(url , headers=headers)
+class GitAutoCommit(FileSystemEventHandler):
+   def __init__(self):
+      self.modifications = {}
 
-   if response.status_code == 200:
-      data = response.json()
-      print(data)
-   else:
-      print(f"Could not get data" , response.status_code)
-   
+   def on_modified(self, event):
+      if not event.is_directory:
+         self.modifications[event.src_path] = "Modified"
 
-def main(token):
-   getData(token)
+   def on_created(self, event):
+      self.modifications[event.src_path] = "Created"
+
+   def on_deleted(self, event):
+      self.modifications[event.src_path] = "Deleted"
+
+   def print(self):
+      print (self.modifications)
+
+   # def get_commit_msg(self):
+   #    root = Tk()
+   #    commit_msg = simpledialog.askstring("GitHub Commit", 
+   #                                           f"Enter commit message:")
+      
+   #    return commit_msg
+
+def main(): 
+   observer = Observer()
+   eventHandler = GitAutoCommit()
+   
+   dir_to_watch = "."
+   
+   observer.schedule(eventHandler, dir_to_watch, recursive=True)
+
+   observer.start()
+
+   try:
+      while True:
+         pass
+   except KeyboardInterrupt:
+      eventHandler.print()
+      observer.stop()
+
+   observer.join()
 
 if __name__ == "__main__":
-   main(PushingToken)
+   main()
